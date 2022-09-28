354c47509108bec4f0e1f043230e561e98a1b9e7
ltGuillaume <ltguillaume@users.noreply.github.com>
2022-09-29 00:39:13 +0200

    Make separate language file lang.php

--- index.php
+++ index.php
@@ -2,35 +2,6 @@
 
 error_reporting(0); // E_ALL
 
-$str["en"] = (object)[
-	"notFound" => "Contents not found!",
-	"wrongWw"  => "Wrong watchword!",
-	"enterWw"  => "Enter the watchword",
-	"submit"   => "Submit",
-	"rTime"    => "Reading time",
-	"rMinutes" => "min",
-	"words"    => "words",
-	"selMode"  => "Selection mode",
-	"setTheme" => "Change theme",
-	"decrSize" => "Decrease font size",
-	"incrSize" => "Increase font size",
-	"jumpPage" => "Jump to page"
-];
-$str["nl"] = (object)[
-	"notFound" => "Inhoud niet gevonden!",
-	"wrongWw"  => "Wachtwoord is onjuist!",
-	"enterWw"  => "Vul het wachtwoord in",
-	"submit"   => "Verzenden",
-	"rTime"    => "Leestijd",
-	"rMinutes" => "min",
-	"words"    => "woorden",
-	"selMode"  => "Selectiemodus",
-	"setTheme" => "Thema",
-	"decrSize" => "Kleinere letters",
-	"incrSize" => "Grotere letters",
-	"jumpPage" => "Naar pagina"
-];
-
 $scriptDir = dirname($_SERVER["SCRIPT_NAME"]);
 $dir       = substr(strtok($_SERVER["REQUEST_URI"], "?"), strlen($scriptDir));
 
@@ -47,6 +18,7 @@ if (is_readable($config))
 if (is_readable("$book/$config"))
 	include "$book/$config";
 
+include "lang.php";
 $lang = $lang ?? substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 0, 2);
 $str  = $str[$lang] ?? $str["en"];
 
